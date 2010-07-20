f4021d31f18526857acfe02816f737f70aaaba49
Alessandro Santos <alpsantos@gmail.com>
2010-07-20 12:57:59 -0300

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Specialized;
+using Common.Logging;
+using Quartz.Impl;
+
+namespace Quartz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var properties = new NameValueCollection();
+            properties["quartz.threadPool.threadCount"] = "1";
+            
+            const string cronExpression = "0/30 * * * * ?";
+
+            var scheduler = new StdSchedulerFactory(properties).GetScheduler();
+            
+            var job = new JobDetail("NomeJob", "GrupoJob", typeof(BillingJob));
+
+            var trigger = new CronTrigger("NomeTrigger", "GrupoTrigger", "NomeJob", "GrupoJob", cronExpression);
+
+            scheduler.ScheduleJob(job, trigger);
+
+            scheduler.Start();
+
+            Console.ReadKey();
+
+            scheduler.Shutdown();
+        }
+    }
+
+    internal class BillingJob : IJob
+    {
+        public void Execute(JobExecutionContext context)
+        {
+            var log = LogManager.GetLogger(typeof(Program));
+            log.Info(string.Format("Executado em : {0}", DateTime.Now));
+        }
+    }
+}
